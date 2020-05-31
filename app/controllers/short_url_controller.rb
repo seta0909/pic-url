c17bb3f91bf1447c98a9c04ce1e7e1930d68d6f8
Seta.Chuang <seta@festground>
2020-05-31 19:40:03 +0800

    fix get short url 404 exception

--- app/controllers/short_url_controller.rb
+++ app/controllers/short_url_controller.rb
@@ -2,12 +2,18 @@ require_relative '../services/short_url_service'
 
 class ShortUrlController < ActionController::API
   def index
-    service = ShortUrlService.new
-    url = service.getUrlFromCode(params[:code])
-    render :json => {
-        'code' => params[:code],
-        'url' => url
-    }
+    begin
+      service = ShortUrlService.new
+      url = service.getUrlFromCode(params[:code])
+      render :json => {
+          'code' => params[:code],
+          'url' => url
+      }
+    rescue ActiveRecord::RecordNotFound => e
+      render :json => {
+          'message' => 'not found short url'
+      }
+    end
   end
 
   def create
